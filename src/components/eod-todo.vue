49974232077bf183f2c8d390defaa5daccb943a8
Dhruv Tilva <tilavadhruv111@gmail.com>
2023-09-20 17:15:40 +0530

    copy button added

--- src/components/eod-todo.vue
+++ src/components/eod-todo.vue
@@ -4,20 +4,21 @@
         <div class="max-w-lg mx-auto">
             <div class="w-full pt-6 space-x-3">
                 <input id="title" type="text" class="w-3/5 border-1 border-black outline-none px-4 py-1"
-                    placeholder="Enter Project Title" v-model="projTitle">
+                    placeholder="Enter Project Title" v-model="projTitle" />
                 <!-- <button
                     class="border-1 border-transparent bg-rose-600 text-white text-sm font-sans font-extrabold px-4 py-[6px]">Add
                     Title</button> -->
             </div>
             <div class="w-full pt-3 flex items-center space-x-3">
                 <input id="task" type="text" class="w-[40%] border-1 border-black outline-none px-4 py-1"
-                    placeholder="Enter Tasks" v-model="listField">
+                    placeholder="Enter Tasks" v-model="listField" />
                 <input id="hr" type="number" class="w-1/4 border-1 border-black outline-none px-4 py-1"
-                    placeholder="Enter Hours" v-model="hour" @keyup.enter="addProjectList">
-                <input type="checkbox" name="" id="" class="inline-block text-xl w-5 h-5" v-model="isComplete">
+                    placeholder="Enter Hours" v-model="hour" @keyup.enter="addProjectList" />
+                <input type="checkbox" name="" id="" class="inline-block text-xl w-5 h-5" v-model="isComplete" />
                 <button @click="addProjectList"
-                    class="border-1 border-transparent bg-rose-600 text-white text-sm font-sans font-extrabold px-4 py-[6px]">Add
-                    Tasks</button>
+                    class="border-1 border-transparent bg-rose-600 text-white text-sm font-sans font-extrabold px-4 py-[6px]">
+                    Add Tasks
+                </button>
             </div>
             <!-- <div class="w-full pt-3 flex items-center space-x-3">
                 <button @click="eodSubmit"
@@ -27,12 +28,20 @@
     </div>
     <div class="mt-5 max-w-xl mx-auto px-8" v-for="(eod, index) in projectArray" :key="index">
         <div>
-            <h3 class="text-xl font-roboto font-bold mb-2">**{{ eod.projTitle }}**</h3>
+            <div class="flex items-center mb-2 space-x-64">
+                <h3 class="text-xl font-roboto font-bold">{{ eod.projTitle }}</h3>
+                <button @click="copyText"
+                    class="text-lg border border-gray-400 px-2 rounded-sm py-1 bg-gray-400 font-roboto font-bold">
+                    Copy
+                </button>
+            </div>
             <ul class="ml-4 font-roboto list-disc font-medium">
-                <li class="flex items-center space-x-2" v-for="(list, index) in eod.taskItems">*&nbsp;{{
-                    list.desc
-                }} {{ list.hr }}h<i class="fa-solid text-lg ml-1"
-                        :class="{ 'text-green-500 fa-check': list.isDone, 'text-yellow-500 fa-clock': !list.isDone }"></i>
+                <li class="flex items-center space-x-2" v-for="(list, index) in eod.taskItems">
+                    {{ list.desc }} {{ list.hr }}h <span v-if="list.isDone">✅</span> <span v-else>⏳</span>
+                    <!-- <i class="fa-solid text-lg ml-1" :class="{
+                        'text-green-500 fa-check': list.isDone,
+                        'text-yellow-500 fa-clock': !list.isDone,
+                    }"></i> -->
                 </li>
             </ul>
         </div>
@@ -49,44 +58,71 @@ export default {
             isComplete: false,
             projectObject: {
                 projTitle: "",
-                taskItems: []
+                taskItems: [],
             },
             projectArray: [],
-            lists: []
-        }
+            lists: [],
+        };
     },
     methods: {
         addProjectTitle() {
             if (this.projTitle) {
                 this.projectObject = {
                     projTitle: "",
-                    taskItems: []
-                }
-                this.projectObject.projTitle = this.projTitle
+                    taskItems: [],
+                };
+                this.projectObject.projTitle = this.projTitle;
             }
         },
         addProjectList() {
             if (this.listField && this.hour) {
-                this.addProjectTitle()
+                this.addProjectTitle();
                 if (this.projTitle) {
-                    this.projectArray.push(this.projectObject)
-                    this.projTitle = ""
+                    this.projectArray.push(this.projectObject);
+                    this.projTitle = "";
                 }
                 if (!this.projTitle) {
                     this.projectArray[this.projectArray.length - 1].taskItems.push({
                         desc: this.listField,
                         hr: this.hour,
-                        isDone: this.isComplete
-                    })
-                    this.listField = ""
-                    this.hour = ""
+                        isDone: this.isComplete,
+                    });
                     this.isComplete = false
+                    this.listField = "";
+                    this.hour = "";
+                    // this.isComplete = false;
                 }
                 // console.log(this.projectArray);
             }
-        }
-    }
-}
+        },
+        copyText() {
+            const div = document.createElement("div");
+            const title = document.createElement("h3");
+            title.innerText = "**" + this.projectObject.projTitle + "**"
+            const ul = document.createElement("ul");
+            this.projectObject.taskItems.forEach((item) => {
+                const emoji = item.isDone ? "✅" : "⏳"
+                const li = document.createElement("li");
+                li.innerText = "* " + item.desc + " " + item.hr + "h " + emoji;
+                ul.appendChild(li);
+                div.appendChild(title);
+                div.appendChild(ul);
+
+                document.body.appendChild(div);
+
+                const selection = window.getSelection();
+                const range = document.createRange();
+                range.selectNodeContents(div);
+                selection.removeAllRanges();
+                selection.addRange(range);
+
+                document.execCommand('copy');
+
+                document.body.removeChild(div);
+            });
+        },
+    },
+};
 </script>
 
 <style></style>
